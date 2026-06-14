5734f0115739dc9a560512236984bc86e3199783
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-13 23:48:14 -0500

    JJBA: Parte 2 - Primer componente - Se crea el componente Pelicula  con una prop "titulo" y la muestra en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
+function Pelicula({ titulo }) {
+  return (
+    <div style={{ border: "1px solid gray", padding: "10 px", margin: "5px" }}>
+      <p>{titulo}</p>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>Mis Películas Favoritas</h1>
+      <Pelicula titulo="El Señor de los Anillos" />
+      <Pelicula titulo="Matrix" />
     </div>
   );
 }
